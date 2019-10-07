1a641460dc2ea3aabbee5581312cbd97686f0d1d
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 15:12:16 -0700

    add bash script version to ssh in

---
+++ bandit/0.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# password:
+# bandit0
+
+# step by step:
+# cat readme
+
+# answer:
+# boJ9jbbUNNfktd78OOpsqOltutMc3MY1
+
+sshpass -p 'bandit0' ssh bandit0@bandit.labs.overthewire.org -p 2220
