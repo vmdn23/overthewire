bb204bed521417eff89e438507634693dc641921
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 15:10:24 -0700

    remove python version

--- bandit/0.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-# ssh
-# ssh bandit0@bandit.labs.overthewire.org -p 2220
-# password bandit0
-# cat readme
-# boJ9jbbUNNfktd78OOpsqOltutMc3MY1
-
-ssh bandit0@bandit.labs.overthewire.org -p 2220
