d8f5a4c7b7398294aace4d1d3180cbe1f54f041c
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 15:13:01 -0700

    clean up syntax

--- bandit/2.sh
+++ bandit/2.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash 
 
-# password: CV1DtqXWVFXTvM2F0k09SHz1YwRINYA9
-# cat spaces\ in\ this\ filename  
-# answer: UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
+# password:
+#CV1DtqXWVFXTvM2F0k09SHz1YwRINYA9
+
+# step by step:
+# cat spaces\ in\ this\ filename
+
+# answer:
+# UmHadQclWmgdLOKQ3YNgjWxGoRMb5luK
 
 sshpass -p 'CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9' ssh bandit2@bandit.labs.overthewire.org -p 2220
