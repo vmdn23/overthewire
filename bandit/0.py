2311d0564260fc529b685823bd2754cd4cbbc4ab
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 13:10:20 -0700

    add addition step and key

--- bandit/0.py
+++ bandit/0.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
+
 # ssh
+# ssh bandit0@bandit.labs.overthewire.org -p 2220
 # password bandit0
+# cat readme
+# boJ9jbbUNNfktd78OOpsqOltutMc3MY1
 
 ssh bandit0@bandit.labs.overthewire.org -p 2220
 
