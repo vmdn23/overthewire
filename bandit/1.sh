7f6334d9b38878acf0ff6a766701efa0a72c616d
vmdn23 <victormdnguyen88@gmail.com>
2019-10-07 13:51:22 -0700

    bash script to ssh in

---
+++ bandit/1.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash 
+
+# passowrd: boJ9jbbUNNfktd78OOpsqOltutMc3MY1
+# cat /home/bandit1/-
+# CV1DtqXWVFXTvM2F0k09SHz0YwRINYA9
+
+sshpass -p 'boJ9jbbUNNfktd78OOpsqOltutMc3MY1' ssh bandit1@bandit.labs.overthewire.org -p 2220
